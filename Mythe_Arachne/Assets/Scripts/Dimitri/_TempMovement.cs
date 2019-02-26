e3508c9d9cb539fa157c99c9136255449098cdbf
DimitrivanManen <17382@ma-web.nl>
2019-02-26 09:54:22 +0100

    Added drawing mechanic

---
+++ Mythe_Arachne/Assets/Scripts/Dimitri/_TempMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _TempMovement : MonoBehaviour {
+
+	public float Speed;
+	public float jumpHeight;
+	private Rigidbody2D playerMove;
+
+	void Start()
+	{
+		playerMove = GetComponent<Rigidbody2D>();
+	}
+
+	void Update()
+	{
+		if(Input.GetKey(KeyCode.A)){
+			playerMove.AddForce(-transform.right * Speed, ForceMode2D.Force);
+		}
+        if (Input.GetKey(KeyCode.D))
+        {
+            playerMove.AddForce(transform.right * Speed, ForceMode2D.Force);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerMove.AddForce(transform.up * jumpHeight, ForceMode2D.Impulse);
+        }
+	}
+
+}
