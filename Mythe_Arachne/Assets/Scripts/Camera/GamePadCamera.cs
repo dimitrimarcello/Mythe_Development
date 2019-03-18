ac8dc4bf09f76efdd644f8a688860c58f8a6ba35
Sjors Kaagman <sjorspnk@gmail.com>
2019-03-18 21:46:25 +0100

    A version of the gamepad camera not sure if its definitive

--- Mythe_Arachne/Assets/Scripts/Camera/GamePadCamera.cs
+++ Mythe_Arachne/Assets/Scripts/Camera/GamePadCamera.cs
@@ -2,14 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GamePadCamera : MonoBehaviour {
-
-    [SerializeField]GameObject Player;
+public class GamePadCamera : MonoBehaviour
+{
+    [SerializeField] GameObject Player;
     PlayerInput pInput;
+    float Speed = 0;
 
     private void FixedUpdate()
     {
-        transform.position = Player.transform.position;
+        SmoothMover();
         transform.rotation = Quaternion.Euler(0, 0, pInput.GyroInput.z);
+    }
+
+    void SmoothMover()
+    {
+        if (Mathf.Round(transform.position.x) != Mathf.Round(Player.transform.position.x) || Mathf.Round(transform.position.y) != Mathf.Round(Player.transform.position.y))
+        {
+            Speed += 0.01f / 100;
+            transform.position = Vector3.Lerp(transform.position, new Vector3(Player.transform.position.x, Player.transform.position.y, -10), Speed);
+        }
+        else
+        {
+            if (Speed > 0.0001)
+            {
+                Speed -= 0.01f * 10;
+            }
+        }
+        if (Speed > 0.1f)
+        {
+            Speed = 0.1f;
+        }
+        if (Speed < 0.0001)
+        {
+            Speed = 0.0001f;
+        }
     }
 }
