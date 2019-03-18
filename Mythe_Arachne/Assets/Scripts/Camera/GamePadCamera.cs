214fabe5b476dff82c54a6953c9a782fedea55a8
Sjors Kaagman <sjorspnk@gmail.com>
2019-03-18 20:34:01 +0100

    TV Camera feels perfect right now if you ask me. Working on gamepad camera.

---
+++ Mythe_Arachne/Assets/Scripts/Camera/GamePadCamera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePadCamera : MonoBehaviour {
+
+    [SerializeField]GameObject Player;
+    PlayerInput pInput;
+
+    private void FixedUpdate()
+    {
+        transform.position = Player.transform.position;
+        transform.rotation = Quaternion.Euler(0, 0, pInput.GyroInput.z);
+    }
+}
