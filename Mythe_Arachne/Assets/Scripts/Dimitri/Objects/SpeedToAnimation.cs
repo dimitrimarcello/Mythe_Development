b4334edc3e658cf2057edad30fafd7cd146006da
DimitrivanManen <17382@ma-web.nl>
2019-03-21 22:21:48 +0100

    Implemented animations and temp movement to the player

---
+++ Mythe_Arachne/Assets/Scripts/Dimitri/Objects/SpeedToAnimation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedToAnimation : MonoBehaviour {
+
+	public Animator setValues;
+	private Rigidbody2D getValues;
+
+	void Awake()
+	{
+		getValues = GetComponent<Rigidbody2D>();
+	}
+
+	void Update()
+	{
+		setValues.SetFloat("VelX", Input.GetAxis("Horizontal"));
+        setValues.SetFloat("VelY", getValues.velocity.y);
+	}
+
+}
