302cacef5ded96cee8faa13381dbe87f08866dd0
Spraxs <jaimy.putter@outlook.com>
2019-04-01 13:26:31 +0200

    Created custom button collider and swing animator

---
+++ Mythe_Arachne/Assets/ButtonCollider.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonCollider : MonoBehaviour {
+
+    private Image image;
+    private Button button;
+
+    [SerializeField]
+    private Text text;
+
+    void Awake()
+    {
+        image = GetComponent<Image>();
+        button = GetComponentInParent<Button>();
+
+        Color color = image.color;
+
+        color.a = 0f;
+
+        image.color = color;
+
+        if (!button.interactable)
+        text.color = Color.gray;
+
+    }
+
+    void Start()
+    {
+
+    }
+
+    public void OnPointerUp()
+    {
+        if (!button.interactable) return;
+        text.color = Color.white;
+    }
+
+    public void OnPointerDown()
+    {
+        if (!button.interactable) return;
+        text.color = Color.yellow;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Mythe_Arachne/Assets/SwingAnimation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwingAnimation : MonoBehaviour {
+
+    private Image image;
+
+    [SerializeField]
+    private float speed;
+
+    private float movement;
+
+    [SerializeField]
+    private float maxRotation;
+
+	// Use this for initialization
+	void Start () {
+        image = GetComponent<Image>();
+    }
+	
+	void FixedUpdate () {
+
+        transform.rotation = Quaternion.Euler(0f, 0f, maxRotation * Mathf.Sin(Time.time * speed));
+
+	}
+}
