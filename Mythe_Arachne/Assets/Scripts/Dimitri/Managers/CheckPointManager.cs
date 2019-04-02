02454f5989d02788b027e617b6866a8e5b6b1e04
fgdhsfgb bgggbbgfb <vmanen68@gmail.com>
2019-04-02 13:37:38 +0200

    Added CheckpointManager to keep track of checkpoints and gets last visited checkpoint

---
+++ Mythe_Arachne/Assets/Scripts/Dimitri/Managers/CheckPointManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPointManager : MonoBehaviour {
+
+    public List<Checkpoint> checkPoints = new List<Checkpoint>();
+    [SerializeField]
+    private float closingDistance = 3;
+    public Checkpoint lastCheckpoint;
+    public Transform player;
+    public Action CheckPointChanged;
+
+    private void Start()
+    {
+        Checkpoint[] currents = GameObject.FindObjectsOfType<Checkpoint>();
+        foreach(Checkpoint current in currents)
+        {
+            checkPoints.Add(current);
+        }
+    }
+
+    private void Update()
+    {
+        CheckForClosePoint();
+    }
+
+    public void CheckForClosePoint()
+    {
+        foreach(Checkpoint point in checkPoints)
+        {
+            if (Vector3.Distance(point.checkPointPos, player.position) < closingDistance)
+            {
+                lastCheckpoint = point;
+                if(CheckPointChanged != null)
+                {
+                    CheckPointChanged();
+                }
+            }
+        }
+    }
+
+}
