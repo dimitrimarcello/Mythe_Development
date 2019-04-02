fb7986f24b803872e906323dbd5e585dd0710f99
fgdhsfgb bgggbbgfb <vmanen68@gmail.com>
2019-04-02 11:55:24 +0200

    Image meta

--- Mythe_Arachne/Assets/Scripts/Dimitri/Objects/MouseFolow.cs
+++ Mythe_Arachne/Assets/Scripts/Dimitri/Objects/MouseFolow.cs
@@ -2,7 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MouseObject : MonoBehaviour {
+public class MouseFolow : MonoBehaviour {
 
+	private InteractManager getData;
+	public float zDepth;
+
+	void Start()
+	{
+		getData = GameObject.FindObjectOfType<InteractManager>();
+	}
+
+	void Update()
+	{
+		Vector3 folowPos = getData.currentCamera.ScreenToWorldPoint(Input.mousePosition);
+		folowPos.z = zDepth;
+		transform.position = folowPos;
+	}
 	
 }
