9a68bbb0fd6354814c52043b05e5e0afa1f0f301
Sjors Kaagman <sjorspnk@gmail.com>
2019-04-15 11:38:16 +0200

    Animation speed based on joystick range

---
+++ Mythe_Arachne/Assets/Scripts/AnimatorSpeed.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorSpeed : MonoBehaviour {
+
+    [SerializeField] PlayerInput input;
+    [SerializeField] Animator CharacterSpeed;
+
+    private void Start()
+    {
+        input = GetComponent<PlayerInput>();
+        CharacterSpeed = GetComponent<Animator>();
+    }
+
+    void FixedUpdate () {
+        if (input.JoystickMove.x > 0 || input.JoystickMove.x < 0)
+        {
+            CharacterSpeed.speed = Mathf.Abs(input.JoystickMove.x);
+        }
+        else
+        {
+            CharacterSpeed.speed = 1;
+        }
+	}
+}
