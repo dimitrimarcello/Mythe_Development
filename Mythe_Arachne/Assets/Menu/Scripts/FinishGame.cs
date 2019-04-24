df9bde87bb9db923ff8865e8e37322dbb90ccc6f
Spraxs <jaimy.putter@outlook.com>
2019-04-24 23:23:33 +0200

    Added finish screen

---
+++ Mythe_Arachne/Assets/Menu/Scripts/FinishGame.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishGame : MonoBehaviour {
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer != 12) return;
+
+        SceneManager.LoadScene(2);
+    }
+}
